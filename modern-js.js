28b33c205e2dedd47cc742804aeb6b924c69ce70
Kajaporn Puttarak <kanjaporn.put@dome.tu.ac.th>
2026-09-16 11:16:20 +0700

    first lab 5

--- modern-js.js
+++ modern-js.js
@@ -6,3 +6,38 @@ const greet_modern = (name, faculty) => { `สวัสดี ${name} จาก�
 
 console.log(greet("Pong", "IT"));
 console.log(greet_modern("Pong", "IT"));
+
+const student = { name : "ฟ้า", faculty : " CITU ", year : 2};
+
+const updated = { ...student, year : 3};
+
+console.log(updated);
+const scores = [90, 80, 70, 60, 50];
+const buses =[
+    { route: "NGV-1", passengers: 45, late: false },
+    { route: "NGV-2", passengers: 62, late: true },
+    { route: "NGV-3", passengers: 38, late: true },
+    { route: "NGV-4", passengers: 51, late: false }
+];
+
+for (let i = 0; i < buses.length; i++) {
+   // console.log(buses[i].route, buses[i].passengers, buses[i].late);
+}
+
+for(const bus of buses) {
+    console.log(bus.route, bus.passengers, bus.late);
+}
+const routes = buses.map(bus => bus.route);
+
+console.log(routes);
+
+const lateBuses = buses.filter(({late}) => late === true).map(({route}) => route);
+const heavyBuses = buses.filter(({passengers}) => passengers > 50);
+
+console.log(lateBuses);
+console.log(heavyBuses);
+
+const totalPassengers = buses.reduce(
+    (total, {passengers}) => total + passengers, 0
+);
+console.log(totalPassengers);
